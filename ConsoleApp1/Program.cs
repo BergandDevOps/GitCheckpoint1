df507c1313653e24fbf0f6218ee84444e33e73cf
BergandDevOps <emiander92@gmail.com>
2023-02-16 14:10:30 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Git checkpoint task 1.1");
+        }
+    }
+}
